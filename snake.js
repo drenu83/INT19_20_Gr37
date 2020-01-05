27e500c4d79075d7acf5edb9140c0984419d7fb9
ArditaKras <58491146+ArditaKras@users.noreply.github.com>
2020-01-05 22:39:44 +0100

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,166 @@
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// Krijoni njesine
+const box = 32;
+
+// Fotot
+
+const ground = new Image();
+ground.src = "ground.png";
+
+const foodImg = new Image();
+foodImg.src = "food.png";
+
+// audio files
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "dead.mp3";
+eat.src = "eat.mp3";
+up.src = "up.mp3";
+right.src = "right.mp3";
+left.src = "left.mp3";
+down.src = "down.mp3";
+
+// Per krijimin e gjarperit
+
+let snake = [];
+
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+
+// Per krijiin e ushqimit
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// Krijimi i scorjeve
+
+let score = 0;
+
+//Kontrollimi igjarprit
+
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
+        left.play();
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+        up.play();
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+        right.play();
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+        down.play();
+    }
+}
+
+
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+function draw(){
+    
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "green" : "white";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // pozicioni i vjeter i kokes
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // ncilin drejtim me leviz
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // nese gjarpri e han ushqimin
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        eat.play();
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // nuk e fshijme bishtin
+    }else{
+        // fshije bishtin
+        snake.pop();
+    }
+    
+    // shto koken e re
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // game over
+    
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+        dead.play();
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score,2*box,1.6*box);
+}
+
+// thirr draw function qdo 100 ms
+
+let game = setInterval(draw,100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
